551148e3dd11e940f0560016a2cefce522069471
Tirth <tirthacodes@gmail.com>
2023-08-08 19:12:02 +0545

    documentation generated using Swagger

--- src/task/dtos/CreateTask.dto.ts
+++ src/task/dtos/CreateTask.dto.ts
@@ -1,15 +1,19 @@
 import { IsNumber, IsString, IsBoolean, IsNotEmpty } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger/dist/decorators";
 
 export class CreateTaskDto{
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     title: string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     description: string;
 
     @IsBoolean()
+    @ApiProperty()
     completed: boolean;
 }
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Put, Delete, Body, Param, HttpException, HttpSta
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dtos/CreateTask.dto';
 import { UpdateTaskDto } from './dtos/UpdateTask.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger/dist';
 
+@ApiTags('task')
 @Controller('task')
 export class TaskController {
     constructor(private taskService: TaskService){
@@ -10,12 +12,17 @@ export class TaskController {
     }
 
     @Get('')
+    @ApiOperation({ summary: 'Get all tasks' })
+    @ApiResponse({ status: 200, description: 'Success' }) 
     getTasks(){
         const tasks = this.taskService.listTasks();
         return tasks;
     }
 
     @Get(':id')
+    @ApiOperation({ summary: 'Get task by ID' })
+    @ApiResponse({ status: 200, description: 'Success' })
+    @ApiResponse({ status: 404, description: 'Task not found' })
     async getById(@Param('id') taskId: string){
         const task = await this.taskService.getTaskById(taskId);
         if (!task) {
@@ -25,17 +32,25 @@ export class TaskController {
     }
 
     @Post('create')
+    @ApiOperation({ summary:'Create a new task' }) 
+    @ApiResponse({ status: 201, description: 'Task created successfully' })
     createTask(@Body() createTaskDto: CreateTaskDto){
         return this.taskService.createTask(createTaskDto);
     }
 
     @Put(':id')
+    @ApiOperation({ summary: 'Update task by ID' }) 
+    @ApiResponse({ status: 200, description: 'Success' }) 
+    @ApiResponse({ status: 404, description: 'Task not found' })
     updateTask(@Param('id') taskId: string, @Body() updatedTask: UpdateTaskDto){
         const task = this.taskService.updateTaskById(taskId, updatedTask);
         return task;
     }
 
     @Delete(':id')
+    @ApiOperation({ summary: 'Delete task by ID' }) 
+    @ApiResponse({ status: 200, description: 'Success' }) 
+    @ApiResponse({ status: 404, description: 'Task not found' })
     deleteTask(@Param('id') taskId: string){
         const task = this.taskService.deleteTaskById(taskId);
         return task;
