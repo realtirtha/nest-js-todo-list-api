06c09a03df0cc8a7884dc16a9312459d5f514726
Tirth <tirthacodes@gmail.com>
2023-08-09 12:58:54 +0545

    Api Property added on UpdatedTask DTO

--- src/task/dtos/UpdateTask.dto.ts
+++ src/task/dtos/UpdateTask.dto.ts
@@ -1,11 +1,14 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateTaskDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   title: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   description: string;
 }
